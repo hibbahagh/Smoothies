90890467b181d2ed8d04365095eeb1ff7d2b4fee
Apple <apple@Apples-MacBook-Pro-2.local>
2019-06-18 13:07:11 -0500

    intitial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+fetch('https://fe-apps.herokuapp.com/api/v1/whateverly/1811/lizAsbell/smoothies')
+.then(response => response.json())
+.then(data => data.smoothies)
+.then(smoothies => appendSmoothies(smoothies))
+
+function appendSmoothies(smoothies) {
+  smoothies.forEach(function(smoothies) {
+ $('article').append(`<p>${smoothies.name}</p>`)
+
+  });
+}
